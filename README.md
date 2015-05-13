70ab68135f99f156bc47ed377b1a62e241f491e5
WuYinwen <wuyinwen@WuYinwens-MacBook-Air.local>
2015-05-13 00:47:59 -0400

    updated version

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Weather Predictor
+
+A weather forecast website application that shows the future 3-days weather forecast summary when user type in a city name.
+
+Involving Bootstrap, PHP, jQuery, HTML.
+
+the website application can be visited at: 
+http://217.199.187.195/billtestweb.com/weather/index_weather.php
+
+the weather information was scraped from weather-forecast.com by using PHP.
--- scraper.php
+++ scraper.php
@@ -1,5 +1,5 @@
 <?php
-
+// this PHP script is used to get the weather infomation from weather-forecast.com
 
 $input_city = $_GET["city"];
 
@@ -7,11 +7,7 @@ $input_city = str_replace(" ", "-", $input_city);
 
  $meta_content = file_get_contents("http://www.weather-forecast.com/locations/".$input_city."/forecasts/latest");
 
- //preg_match("/3 Day Weather Forecast Summary:/i", $meta_content, $matched_content);
-
  preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)<\/span>/s', $meta_content, $matched_content);
-
- //print_r($matched_content);
 
  echo $matched_content[1];
 
